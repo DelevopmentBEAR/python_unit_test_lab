94b9ff4e759f9fb8541667fb1e20723674fb6b1e
Berit Haugen <movies.tv.and.music@gmail.com>
2026-09-23 19:42:11 -0500

    Addtional comments for tests for discount.

--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -4,30 +4,38 @@ from price_discount import discount
 
 class TestDiscount(TestCase):
 
+    # Can it handle three prices and one discount?
     def test_list_of_three_prices(self):
         prices = [10, 4, 20]
         expected_discount = 4
         calculated_discount = discount(prices)
         self.assertEqual(expected_discount, calculated_discount)
         
+    # Can it handle only one price and no discounts?    
     def test_list_of_one_price_no_discount_raise_exception(self):
         self.fail('finish this test')
         
+    # Is the price an int? If not, raise exception.    
     def test_list_cotains_non_int_values_raise_exception(self):
         self.fail('finish this test')
     
+    # Are the prices in a list, if so can it be handled? If not, raise exception.
     def test_argument_is_list_raise_exception(self):
         self.fail('finish this test')
 
+    # Is price a number/int greater than zero?
     def test_list_contains_num_greater_than_zero(self):
         self.fail('finish this test')
         
+    # Is price a negative number/zero? If it doesn't work, raise exception.
     def test_list_contains_zero_or_negative_num_raise_exception(self):
         self.fail('finish this test')
 
+    # Does the price hit a max number?
     def test_max_num__raise_exception(self):
         self.fail('finish this test')
     
+    # What is the min number for price?
     def test_min_num_raise_exception(self):
         self.fail('finish this test')
         
